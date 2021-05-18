88f1c91b3008542f638140210072d9347ba5eccf
JoJo Myles <joneshiamyles31@gmail.com>
2021-05-18 10:07:58 -0400

    inital commit

---
+++ src/array_prac/Runner.java
@@ -0,0 +1,39 @@
+package array_prac;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		// the maximum index in an array
+		//is the size - 1
+		
+		
+		//array declaration
+		int[] intArray ;
+		
+		//if intialize on a separate line, it cannot be initialized
+		//it with values
+		
+		intArray =  new int[3];
+		
+		
+		//adding values to an array
+		intArray[0] = 34;
+		intArray[1] = 43;
+		intArray[2] = 48;
+		
+		
+		//declare and intializing with values
+				int[] otherIntArray = {1, 2, 5 };
+				
+		// Get some values from  the array
+		int someNum = otherIntArray[2];
+		System.out.println(someNum);
+		
+		
+		
+		
+		}
+	}
+
+
